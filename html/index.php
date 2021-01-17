88ae52fbd755be16277f0d771e1d34e2859c105c
SOUnit <workbookwork7@gmail.com>
2021-01-17 12:39:10 +0900

    added initial files for php setup

---
+++ html/index.php
@@ -0,0 +1,21 @@
+<!--
+    setup page 
+    https://qiita.com/naente_dev/items/d259ea84c172deeff7d8 
+-->
+
+<?php $test = 10; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Sample</title>
+</head>
+
+<body>
+    <h1>Test</h1>
+    <p><?php echo $test ?></p>
+</body>
+
+</html>
